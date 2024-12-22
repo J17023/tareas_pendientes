8dc3331d7880c624054792e8b7ef5ca1c41409c2
j17023 <josejulian1723@gmail.com>
2024-12-22 00:47:32 -0500

    se creo un ejemplo basico

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.proyecto_basico"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.proyecto_basico"
--- app/src/main/java/com/example/proyecto_basico/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_basico/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.proyecto_basico
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val duration: Int = Toast.LENGTH_SHORT
+        val editar_texto: EditText = findViewById(R.id.Editar_texto)
+        val boton_presion:Button = findViewById(R.id.Presionar)
+
+
+
+        boton_presion.setOnClickListener{
+            val texto_toast = editar_texto.text.toString()
+
+            if(texto_toast.isEmpty()){
+                Toast.makeText(this,"No se coloco texto",duration).show()
+            }else{
+                Toast.makeText(this,texto_toast,duration).show()
+            }
+
         }
     }
 }
